1f09fc021c9e16b996d6b48aeea5f91576c81969
Mario Fliegner <mario.fliegner@aqipro.com>
2026-05-26 11:45:30 +0200

    Rename internal settings storage to SameView

--- app/src/main/java/com/isardomains/ghostshot/ui/settings/SettingsModule.kt
+++ app/src/main/java/com/isardomains/ghostshot/ui/settings/SettingsModule.kt
@@ -25,6 +25,6 @@ object SettingsModule {
             corruptionHandler = ReplaceFileCorruptionHandler(
                 produceNewData = { emptyPreferences() }
             ),
-            produceFile = { context.preferencesDataStoreFile("then_and_now_camera_settings") }
+            produceFile = { context.preferencesDataStoreFile("sameview_settings") }
         )
 }
