296822ef020b4b24578b3178bb231194ba010d72
Mario Fliegner <mario.fliegner@aqipro.com>
2026-05-26 11:25:10 +0200

    Update user-facing branding to SameView

--- app/src/main/java/com/isardomains/ghostshot/AppConstants.kt
+++ app/src/main/java/com/isardomains/ghostshot/AppConstants.kt
@@ -1,5 +1,5 @@
 package com.isardomains.ghostshot
 
 internal object AppConstants {
-    const val CAPTURE_EXIF_SOFTWARE = "Then & Now Camera by IsarDomains.com"
+    const val CAPTURE_EXIF_SOFTWARE = "SameView"
 }
