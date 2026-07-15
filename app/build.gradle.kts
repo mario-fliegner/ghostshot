b85763cc05483ca03a5d5244f230ca6574639cbd
Mario Fliegner <mario.fliegner@aqipro.com>
2026-07-15 08:17:20 +0200

    Updated versionCode for RC

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.isardomains.sameview"
         minSdk = 29
         targetSdk = 35
-        versionCode = 3
+        versionCode = 100
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
